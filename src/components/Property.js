3f1ee393951fc406cf2843acd811576d35264f03
Michael S <mihai.sanfran@gmail.com>
2019-01-19 18:46:43 -0700

    Implemented Google Maps directions

--- src/components/Property.js
+++ src/components/Property.js
@@ -1,43 +1,44 @@
 import React, { Component } from 'react';
-import { ScrollView, View, Text } from 'react-native';
+import { ScrollView, View, Button } from 'react-native';
+
+import getDirections from 'react-native-google-maps-directions'
 
 
 class Property extends Component {
-   // Initialize state
-   state = { property: [] };
 
-   // Anytime that the component is about to render this lifecycle method will be called
-   componentWillMount() {
-       /*
-       // Returns a promise that .then will be called once the http call is complete
-       var bodyFormData = new FormData();
-       bodyFormData.append('data', 'all');
-
-       axios({
-           method: 'post',
-           url: 'http://ec2-34-209-228-103.us-west-2.compute.amazonaws.com/properties.php',
-           data: bodyFormData,
-           config: { headers: {'Content-Type': 'multipart/form-data' }}
-           })
-           .then(response => this.setState({ properties: response.data.property }))
-           .catch(function (response) {
-               console.log(response);
-           }); 
-           */
-   }
-
-    renderProperty() {
-        // Map over array of albums and return one item detail component
-        return <Text>Property Details</Text>
-    }
+    handleGetDirections = () => {
+        const data = {
+           source: {
+            latitude: -33.8356372,
+            longitude: 18.6947617
+          },
+          destination: {
+            latitude: -33.8600024,
+            longitude: 18.697459
+          },
+          params: [
+            {
+              key: "travelmode",
+              value: "driving"        // may be "walking", "bicycling" or "transit" as well
+            },
+            {
+              key: "dir_action",
+              value: "navigate"       // this instantly initializes navigation using the given travel mode 
+            }
+          ]
+        }
+     
+        getDirections(data)
+      }
+     
 
     render() {
         console.log(this.state);
 
-        return ( // Everything inside this view is scrollable
-            <ScrollView>
-                {this.renderProperty()}
-            </ScrollView>
+        return (
+            <View>
+                <Button onPress={this.handleGetDirections} title="Get Directions" />
+            </View>
         );
     }
 }
