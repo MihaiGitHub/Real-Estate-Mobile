b1c7237d8411ad2eaf235eca277215021664b5df
Michael S <mihai.sanfran@gmail.com>
2019-02-10 17:37:19 -0700

    Added bottom tab navigation with react native router flux

--- src/Router.js
+++ src/Router.js
@@ -1,24 +1,50 @@
 import React from 'react';
+import { Text } from 'react-native';
+
 import { Scene, Router, Actions } from 'react-native-router-flux';
 import PropertyList from './components/PropertyList';
 import Property from './components/Property';
 //import PropertyDetail from './src/components/PropertyDetail';
 
+const Properties = ({ selected, title }) => {
+    return (
+      <Text style={{color: selected ? 'red' :'black'}}>{title}</Text>
+    );
+}
+
+const Agents = ({ selected, title }) => {
+    return (
+        <Text style={{color: selected ? 'red' :'black'}}>{title}</Text>
+    );
+}
+
+const Profile = ({ selected, title }) => {
+    return (
+        <Text style={{color: selected ? 'red' :'black'}}>{title}</Text>
+    );
+}
+
 const RouterComponent = () => {
     return (
         <Router>
             <Scene key="root" hideNavBar>{/* Hide extra header due to nested scenes */}
-                 
-                <Scene key="main">
-                    <Scene 
-                        initial
-                  //      rightTitle="Add" 
-                  //      onRight={() => Actions.employeeCreate()} 
-                        key="propertyList" 
-                        component={PropertyList} 
-                        title="Property List" />
+                {/* Tab Container */}
+                <Scene key="tabbar" tabs={true} tabBarStyle={{ backgroundColor: '#FFFFFF' }}>
+                    {/* Tab and it's scenes */}
+                    <Scene key="osu" title="Properties" icon={Properties}>
+                        <Scene initial key="propertyList" component={PropertyList} title="Property List" />
+                        <Scene key="propertyView" component={Property} title="View Property" />
+                    </Scene>
 
-                    <Scene key="propertyView" component={Property} title="View Property" />
+                    {/* Tab and it's scenes */}
+                    <Scene key="um" title="Agents" icon={Agents}>
+                        <Scene key="agentList" component={PropertyList} title="Agent List" />
+                    </Scene>
+
+                    {/* Tab and it's scenes */}
+                    <Scene key="vu" title="My HomeSquare" icon={Profile}>
+                        <Scene key="profile" component={PropertyList} title="My HomeSquare" />
+                    </Scene>
                 </Scene>
             </Scene>
         </Router>
