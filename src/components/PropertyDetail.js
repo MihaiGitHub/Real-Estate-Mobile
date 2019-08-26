c9bf779675574e3e596c97d087f07d6cf27a5dfe
Mihai Smarandache <mihai.sanfran@gmail.com>
2019-08-26 16:34:15 -0700

    Added property image gallery

--- src/components/PropertyDetail.js
+++ src/components/PropertyDetail.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
-import { Image } from 'react-native';
+import { Image, View } from 'react-native';
 import axios from 'axios';
-import getDirections from 'react-native-google-maps-directions'
+import getDirections from 'react-native-google-maps-directions';
 import { Container, Header, Content, Card, CardItem, Thumbnail, Text, Button, Left, Body } from 'native-base';
 import Icon from 'react-native-vector-icons/FontAwesome5';
+import { SliderBox } from 'react-native-image-slider-box';
 import GLOBALS from './common/Globals';
-import SCREEN_IMPORT from 'Dimensions'
+import SCREEN_IMPORT from 'Dimensions';
 
 const SCREEN_WIDTH = SCREEN_IMPORT.get('window').width;
 
@@ -13,21 +14,23 @@ class PropertyDetail extends Component {
     
     state = {
         property: {},
-        imgUrl: `${GLOBALS.BASE_URL}/dashboard/img/house.gif`
+        images: []
     }
 
     componentWillMount = () => {
         axios.get(`${GLOBALS.BASE_URL}/properties.php?data=propertyDetail&id=${this.props.id}`)
             .then(response => {
-                
                 if(response.data.property[0].pImage){
+                    arr = response.data.property[0].pImage.map(i => `${GLOBALS.BASE_URL}` + i);
+
                     this.setState({
                         property: response.data.property[0],
-                        imgUrl: `${GLOBALS.BASE_URL}/${response.data.property[0].pImage}`
+                        images: arr
                     })
                 } else {
                     this.setState({
-                        property: response.data.property[0]
+                        property: response.data.property[0],
+                        images: [`${GLOBALS.BASE_URL}/dashboard/img/house.gif`]
                     })
                 }  
                 
@@ -56,38 +59,47 @@ class PropertyDetail extends Component {
         }
      
         getDirections(data)
-      }   
+    }
 
     render() {
         return (
             <Container>
                 <Content>
-                <Card style={{flex: 0}}>
-                    <CardItem>
-                    <Body>
-                        <Image 
-                            source={{ uri: this.state.imgUrl }} 
-                            style={{ height: 200, width: SCREEN_WIDTH-40, flex: 1, marginBottom: 5 }} />
-                        <Button 
-                            full
-                            info 
-                            onPress={this.handleGetDirections} 
-                            style={{ marginBottom: 5 }}>
+                    <View style={{flex: 0}}>
+                        <SliderBox
+                            circleLoop={true}
+                            images={this.state.images}
+                            sliderBoxHeight={200}
+                            onCurrentImagePressed={index =>
+                                console.log(`image ${index} pressed`)
+                            }
+                            dotColor="#FFEE58"
+                            inactiveDotColor="#90A4AE"
+                        />
+                    </View>
+                    <Card style={{flex: 0}}>
+                        <CardItem>
+                            <Body>
+                                <Button
+                                    full
+                                    info 
+                                    onPress={this.handleGetDirections} 
+                                    style={{ marginBottom: 5 }}>
                                 <Icon active name="directions" size={25} color="#ffffff" />
                                 <Text>GET DIRECTIONS</Text>
-                        </Button>
-                        <Text style={{ marginTop: 5 }}>
-                            {this.state.property.bedroom} Beds {this.state.property.bathroom} Baths
-                        </Text>
-                        <Text style={{ marginBottom: 5 }}>
-                            {this.state.property.location}
-                        </Text>
-                        <Text>
-                            {this.state.property.description}
-                        </Text>
-                    </Body>
-                    </CardItem>
-                </Card>
+                                </Button>
+                                <Text style={{ marginTop: 5 }}>
+                                    {this.state.property.bedroom} Beds {this.state.property.bathroom} Baths
+                                </Text>
+                                <Text style={{ marginBottom: 5 }}>
+                                    {this.state.property.location}
+                                </Text>
+                                <Text>
+                                    {this.state.property.description}
+                                </Text>
+                            </Body>
+                        </CardItem>
+                    </Card>
                 </Content>
             </Container>
         );
