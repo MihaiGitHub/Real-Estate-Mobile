edc9dacc142bb094c28e65359d8054594910614a
Mihai Smarandache <mihai.sanfran@gmail.com>
2019-08-31 22:28:32 -0700

    Update property list

--- src/components/PropertyList.js
+++ src/components/PropertyList.js
@@ -3,7 +3,6 @@ import { connect } from 'react-redux';
 import { Scene, Router, Actions } from 'react-native-router-flux';
 import { Image } from 'react-native';
 import { Container, Header, Content, Card, CardItem, Thumbnail, Text, Button, Left, Body, Right } from 'native-base';
-import Icon from 'react-native-vector-icons/FontAwesome5';
 import GLOBALS from './common/Globals';
 import { propertiesFetch } from '../actions';
 import { Spinner } from './common/Spinner';
@@ -17,36 +16,18 @@ class PropertyList extends Component {
     renderProperties(){
         return this.props.list.map(property => {
             return (
-                <Card key={property.pId}>
-                    <CardItem>
-                    <Left>
-                        <Body>
-                            <Text>${property.price}</Text>
-                            <Text note>{property.location}</Text>
-                        </Body>
-                    </Left>
-                    </CardItem>
+                <Card key={property.pId} style={{ flex: 0 }}>
                     <CardItem cardBody button onPress={() => Actions.propertyView({ id: property.pId })}>
                         <Image 
                             source={{uri: property.pImage == null ? `${GLOBALS.BASE_URL}/dashboard/img/house.gif` : `${GLOBALS.BASE_URL}${property.pImage}`}} 
                             style={{height: 200, width: null, flex: 1}} />
                     </CardItem>
                     <CardItem>
-                    <Left>
-                        <Button transparent>
-                        <Icon active name="bed" size={20} />
-                        <Text style={{ color: '#808080' }}>{property.bedroom} Beds</Text>
-                        </Button>
-                    </Left>
-                    <Body>
- 
-                    </Body>
-                    <Right>
-                        <Button transparent>
-                            <Icon active name="bath" size={20} />
-                            <Text style={{ color: '#808080' }}>{property.bathroom} Baths</Text>
-                        </Button>
-                    </Right>
+                        <Body>
+                            <Text style={{ fontWeight: '600' }}>${property.price}</Text>
+                            <Text>{property.bedroom} beds   {property.bathroom} baths</Text>
+                            <Text>{property.location}</Text>
+                        </Body>
                     </CardItem>
                 </Card>
             );
