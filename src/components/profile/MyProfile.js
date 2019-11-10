577c6d4d72e4767db21fc75e5b4f478bc11c6253
Mihai Smarandache <mihai.sanfran@gmail.com>
2019-11-09 21:27:54 -0700

    Added property sort feature

--- src/components/profile/MyProfile.js
+++ src/components/profile/MyProfile.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Scene, Router, Actions } from 'react-native-router-flux';
 import { Button, Container, Header, Content, List, ListItem, Text, Left, Body, Right, Switch } from 'native-base';
+import Ionicon from "react-native-vector-icons/Ionicons";
 import Icon from "react-native-vector-icons/FontAwesome";
 
 class MyProfile extends Component {
@@ -10,46 +11,46 @@ class MyProfile extends Component {
                 <Content>
                 <ListItem icon onPress={() => Actions.about()}>
                     <Left>
-                        <Icon name={"info-circle"} size={25} />
+                        <Icon name={"info-circle"} size={24} />
                     </Left>
                     <Body>
                         <Text>About</Text>
                     </Body>
                     <Right>
-                        <Icon name={"arrow-right"} size={25} />
+                        <Ionicon active name="md-arrow-forward" />
                     </Right>
                 </ListItem>
                 <ListItem icon onPress={() => Actions.settings()}>
                     <Left>
-                        <Icon name={"cog"} size={25} />
+                        <Icon name={"cog"} size={23} />
                     </Left>
                     <Body>
                         <Text>Settings</Text>
                     </Body>
                     <Right>
-                        <Icon name={"arrow-right"} size={25} />
+                        <Ionicon active name="md-arrow-forward" />
                     </Right>
                 </ListItem>
                 <ListItem icon onPress={() => Actions.privacy()}>
                     <Left>
-                        <Icon name={"book"} size={25} />
+                        <Icon name={"book"} size={21} />
                     </Left>
                     <Body>
                         <Text>Privacy Policy</Text>
                     </Body>
                     <Right>
-                        <Icon name={"arrow-right"} size={25} />
+                        <Ionicon active name="md-arrow-forward" />
                     </Right>
                 </ListItem>
                 <ListItem icon onPress={() => Actions.terms()}>
                     <Left>
-                        <Icon name={"list-alt"} size={25} />
+                        <Icon name={"list-alt"} size={19} />
                     </Left>
                     <Body>
                         <Text>Terms of Use</Text>
                     </Body>
                     <Right>
-                        <Icon name={"arrow-right"} size={25} />
+                        <Ionicon active name="md-arrow-forward" />
                     </Right>
                 </ListItem>
                 </Content>
--- src/components/properties/PropertyList.js
+++ src/components/properties/PropertyList.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Actions } from 'react-native-router-flux';
-import { Image } from 'react-native';
-import { Container, Content, Card, CardItem, Text, Body } from 'native-base';
+import { Image, View } from 'react-native';
+import { Container, Content, Card, CardItem, Text, Body, Picker, Icon } from 'native-base';
 import GLOBALS from '../common/Globals';
 import { propertiesFetch } from '../../actions';
 import { Spinner } from '../common/Spinner';
 
 class PropertyList extends Component {
+
+    state = {
+        selected: "key3"
+    }
 
     componentWillMount() {
         this.props.propertiesFetch();
@@ -34,6 +38,18 @@ class PropertyList extends Component {
         });
     }
 
+    sortHandler = (value) => {
+        if(value === 'key1'){
+            this.props.listFiltered.sort((a, b) => (a.price > b.price) ? 1 : -1)
+        }
+        
+        if (value === 'key2'){
+            this.props.listFiltered.sort((a, b) => (a.price < b.price) ? 1 : -1) 
+        }        
+
+        this.setState({ selected: value });
+    }
+
     render() {        
         if(this.props.loading){
             return <Spinner size="large" />;
@@ -42,6 +58,24 @@ class PropertyList extends Component {
         return (
             <Container>
                 <Content>
+                    
+                    <Picker
+                    mode="dropdown"
+                    placeholder="Sort Listings"
+                    iosIcon={<Icon name="arrow-down" />}
+                        style={{ 
+                        width: undefined,
+                        height: 30,
+                        color: '#3f51b5',
+                        }}
+                        selectedValue={this.state.selected}
+                        onValueChange={(value) => this.sortHandler(value)}
+                    >
+                    <Picker.Item label="Price (Low to High)" value="key1" />
+                    <Picker.Item label="Price (High to Low)" value="key2" />
+                    <Picker.Item label="Newest Listings" value="key3" />
+                    </Picker>
+                    
                     {this.renderProperties()}
                 </Content>
             </Container>
