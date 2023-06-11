12c0c1560a49fe2688c4a4ca246c8ce0e1cf578b
Mihai Smarandache <mihai.sanfran@gmail.com>
2023-06-11 11:51:51 -0700

    Search Zillow properties feature

--- src/components/Properties/PropertiesList.js
+++ src/components/Properties/PropertiesList.js
@@ -41,20 +41,21 @@ export function PropertiesList() {
   const renderProperties = () => {
     if (Array.isArray(listFiltered) && listFiltered.length > 0) {
       return listFiltered.map((item, index, array) => {
-        // if (item.properties_images.length > 0) {
-        //   const images = item.properties_images.map((image) => {
-        //     return { id: item.id, url: image.url, title: item.title };
-        //   });
+        console.log("item ", item);
+        if (item.properties_images.length > 0) {
+          const images = item.properties_images.map((image) => {
+            return { id: item.id, url: image.url, title: "Property Title" };
+          });
 
-        //   array[index]["images"] = images;
-        // } else {
-        //   array[index]["images"] = [
-        //     {
-        //       url: `https://ssl.cdn-redfin.com/photo/115/bigphoto/382/22304382_0.jpg`,
-        //       title: item.title,
-        //     },
-        //   ];
-        // }
+          array[index]["images"] = images;
+        } else {
+          array[index]["images"] = [
+            {
+              url: `https://ssl.cdn-redfin.com/photo/115/bigphoto/382/22304382_0.jpg`,
+              title: item.title,
+            },
+          ];
+        }
 
         return (
           <Box
@@ -65,10 +66,10 @@ export function PropertiesList() {
           >
             <VStack space="0">
               <Box>
-                {/* <ImageCarousel
+                <ImageCarousel
                   data={array[index]["images"]}
                   openGallery={false}
-                /> */}
+                />
               </Box>
               <HStack
                 alignItems="center"
@@ -105,7 +106,62 @@ export function PropertiesList() {
   };
 
   const renderZillowProperties = () => {
-    return <Text>Zillow Properties full</Text>;
+    if (Array.isArray(propertiesZillow) && propertiesZillow.length > 0) {
+      return propertiesZillow.map((item, index, array) => {
+        const { property } = item;
+        console.log("propertyZillow ", property);
+
+        const images = [
+          {
+            id: property.zpid,
+            url: property.imgSrc,
+            title: property.homeType,
+          },
+        ];
+
+        return (
+          <Box
+            border="1"
+            borderRadius="md"
+            key={index}
+            style={{ borderBottom: 100 }}
+          >
+            <VStack space="0">
+              <Box>
+                <ImageCarousel data={images} openGallery={false} />
+              </Box>
+              <HStack
+                alignItems="center"
+                space={0}
+                justifyContent="space-between"
+              >
+                <Text
+                  style={{
+                    flex: 0.36,
+                    marginLeft: 15,
+                    fontSize: 24,
+                    paddingTop: 10,
+                  }}
+                >
+                  ${property.price}
+                </Text>
+                <Text style={{ flex: 0.36 }}>
+                  {property.bedrooms} Beds {property.bathrooms} Baths
+                </Text>
+              </HStack>
+              <Box px="4" pb="1" pt={0}>
+                {property.address.streetAddress}
+              </Box>
+            </VStack>
+          </Box>
+        );
+      });
+    } else {
+      ToastAndroid.show(
+        "No properties found near this location!",
+        ToastAndroid.LONG
+      );
+    }
   };
 
   console.log("listFiltered ", listFiltered, "Zillow ", propertiesZillow);
