a7714a393d8dc970f34e1f723cdde72806311845
Mihai Smarandache <mihai.sanfran@gmail.com>
2023-06-16 17:33:24 -0700

    Install React Native Maps

--- src/components/Properties/PropertiesMap.js
+++ src/components/Properties/PropertiesMap.js
@@ -1,8 +1,18 @@
 import React from "react";
 import { Text } from "react-native";
+import MapView from "react-native-maps";
 
 export function PropertiesMap() {
-  return <Text>PropertyMap</Text>;
+  return (
+    <MapView
+      initialRegion={{
+        latitude: 37.78825,
+        longitude: -122.4324,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421,
+      }}
+    />
+  );
 }
 
 // import React, { useState, useEffect } from "react";
