297a4e878746c027a731afaded0700265be971fd
Mihai Smarandache <mihai.sanfran@gmail.com>
2023-06-18 13:05:40 -0700

    Display list of properties on map

--- src/components/Properties/PropertiesMap.js
+++ src/components/Properties/PropertiesMap.js
@@ -1,18 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
 import { Text } from "react-native";
-import MapView from "react-native-maps";
+import MapView, { Marker } from "react-native-maps";
+import { useSelector, useDispatch } from "react-redux";
+import { useNavigation } from "@react-navigation/native";
 
 export function PropertiesMap() {
+  const { listFiltered, searchLatLng } = useSelector(
+    (state) => state.properties
+  );
+
+  console.log("listFiltered ", listFiltered);
+
+  // const dispatch = useDispatch();
+  const navigation = useNavigation();
+
+  if (Array.isArray(listFiltered) && listFiltered.length > 0) {
+    var { lat, lng } = listFiltered[0];
+  } else {
+    // var { lat, lng } = searchLatLng;
+
+    var lat = 33.448376;
+    var lng = -112.074036;
+  }
+
   return (
     <MapView
       style={{ minHeight: "100%", minWidth: "100%" }}
       initialRegion={{
-        latitude: 37.78825,
-        longitude: -122.4324,
-        latitudeDelta: 0.0922,
-        longitudeDelta: 0.0421,
-      }}
-    />
+        latitude: parseFloat(lat),
+        longitude: parseFloat(lng),
+        latitudeDelta: 0.7,
+        longitudeDelta: 0.7,
+      }} //onRegionChangeComplete runs when the user stops dragging MapView
+      //   onRegionChangeComplete={(region) => setRegion(region)}
+    >
+      {listFiltered.length > 0 &&
+        listFiltered.map((marker, index) => (
+          <Marker
+            key={index}
+            coordinate={{
+              latitude: parseFloat(marker.lat),
+              longitude: parseFloat(marker.lng),
+            }}
+            title={"$" + marker.price}
+            onCalloutPress={() =>
+              navigation.navigate("Property Info", {
+                id: marker.id,
+              })
+            }
+          />
+        ))}
+    </MapView>
   );
 }
 
