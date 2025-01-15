c470a3960d90155d65daabd73cf48308bde3d6ca
mihai <mihai.sanfran@gmail.com>
2025-01-14 17:15:20 -0700

    Code cleanup

--- App.js
+++ App.js
@@ -1,5 +1,4 @@
 import "react-native-get-random-values";
-import { useState, useEffect, useCallback } from "react";
 import { Provider } from "react-redux";
 import { configureStore } from "@reduxjs/toolkit";
 import PropertiesReducer from "./src/reducers/PropertiesReducer";
